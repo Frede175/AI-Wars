d80f140da61fd38a48be564ba90777644570f3b7
Frede175 <fsr1998@hotmail.com>
2015-06-06 17:29:01 +0200

    Sync

--- Assets/Collector.cs
+++ Assets/Collector.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class Collector : Unit {
 	
 	public float rangeForCollectingMoney = 2f;
-	public WorldObjects deposit;
+	public GameObject gameDeposit;
+	public GameObject gameCommanedCenter;
+	public float maxMoneyLoad;
 	public Vector3 positionDeposit;
 	public Vector3 positionHome;
+	public float money = 0;
+	public bool GoingToBase;
 
 	protected override void Start () {
 		base.Start ();
@@ -15,11 +19,55 @@ public class Collector : Unit {
 
 	protected override void Update () {
 		base.Update();
+		StartCollecting ();
 
 	}
 
 	public void StartCollecting()
 	{
+		Deposit deposit = gameDeposit.GetComponent<Deposit>();
+		if (money == 0 && transform.position != positionDeposit && !GoingToBase) {
+			MoveUnit(positionDeposit);
+		}
+		if (transform.position == positionDeposit && money < maxMoneyLoad && !GoingToBase) {
+			if (deposit.IsOwnedBy(player))
+			{
+				if (!deposit.IsEmpty())
+				{
+					money += deposit.TakeMoney();
+					if (money >= maxMoneyLoad)
+					{
+						money = maxMoneyLoad;
+						GoingToBase = true;
+					}
+
+					if(deposit.IsEmpty()) GoingToBase = true;
+				}
+			}
+		}
+
+		if (GoingToBase && transform.position != positionHome ) {
+			MoveUnit(positionHome);
+		}
+
+		if (transform.position == positionHome && GoingToBase)
+		{
+			CommandCenter commandCenter = gameCommanedCenter.GetComponent<CommandCenter>();
+			if (commandCenter.IsOwnedBy(player))
+			{
+				if (money > 0)
+				{
+					commandCenter.GetMoney(money);
+					money = 0;
+					GoingToBase = false;
+				}
+			}
+		}
+
+		if (deposit.IsEmpty () && money == 0) {
+			Destroy(this);
+		}
+
 
 	}
 
---
+++ Assets/Scripts/Objects/CommandCenter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommandCenter : Building {
+
+
+	protected override void Start()
+	{
+		actions = new string[] {"Buy desposit"};
+	}
+
+	public void GetMoney(float amount)
+	{
+		player.money += (int)amount;
+	}
+
+
+
+
+}
